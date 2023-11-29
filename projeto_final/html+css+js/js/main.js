83471629add84736ac48169c43c58f6a0c271db1
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-11-29 20:06:40 -0300

    preloader js animation

--- projeto_final/html+css+js/js/main.js
+++ projeto_final/html+css+js/js/main.js
@@ -1,11 +1,24 @@
+//variables
 var btnContact = document.querySelector('.jl-btn-contact');
 
+//page prelader
+window.addEventListener('load', function(){
+    var pagePreloader = document.querySelector('.jl-preloader');
+
+    setTimeout(function(){
+        pagePreloader.classList.add('jl-fade-out');
+    }, 3000)
+    //the function setTimeOut is used when pageloader are ready, wait 3 seconds to load the principal page
+});
+
+//opening and closing contact information
 btnContact.addEventListener('click', function(){
-    var boxContact = document.querySelector('.jl-contact-info')
+    var boxContact = document.querySelector('.jl-contact-info');
     
     boxContact.classList.toggle('jl-is-open');
     this.classList.toggle('jl-change-icon');
-    //quando eu clicar em algo, façca, quando eu clicar novamente, refaça- conceito de toggle, por isso nao usamos o add, pq ele é acumulativo em suas ações
+    //when I click on something, do it, when I click again, do it again - concept of toggle, that's why we don't use add, because it is cumulative in its actions
 });
 
-//this serve para acessarmos o elemento do escopo atual
+//this is used to access the element of the current scope
+
