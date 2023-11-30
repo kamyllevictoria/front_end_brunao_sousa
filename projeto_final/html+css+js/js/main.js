fd6827e79f4cb71c9a0fcf5735d0c1507f06d33f
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-11-30 19:13:44 -0300

    btn system

--- projeto_final/html+css+js/js/main.js
+++ projeto_final/html+css+js/js/main.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function(){
     setTimeout(function(){
         pagePreloader.style.display = 'none';
     }, 2000)
-    //we need to create a displey= none to disappear with our element and not let the opacity erase the other components of the screen, that's why we use the display settings within the setTimeOut function
+    //we need to create a displey= none to disappear with our element and not let the opacity erase the other components of the screen, that's why we use the display settings within the setTimeOut function  
 
     //the function setTimeOut is used when pageloader are ready, wait 3 seconds to load the principal page
 });
