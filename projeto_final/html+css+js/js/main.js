e19c17819e7d39e1fae20a437029d3d03d30e87c
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2023-11-29 23:07:31 -0300

    hero grid, image and text

--- projeto_final/html+css+js/js/main.js
+++ projeto_final/html+css+js/js/main.js
@@ -4,10 +4,13 @@ var btnContact = document.querySelector('.jl-btn-contact');
 //page prelader
 window.addEventListener('load', function(){
     var pagePreloader = document.querySelector('.jl-preloader');
+    pagePreloader.classList.add('j-fade-out');
 
     setTimeout(function(){
-        pagePreloader.classList.add('jl-fade-out');
-    }, 3000)
+        pagePreloader.style.display = 'none';
+    }, 2000)
+    //we need to create a displey= none to disappear with our element and not let the opacity erase the other components of the screen, that's why we use the display settings within the setTimeOut function
+
     //the function setTimeOut is used when pageloader are ready, wait 3 seconds to load the principal page
 });
 
@@ -21,4 +24,3 @@ btnContact.addEventListener('click', function(){
 });
 
 //this is used to access the element of the current scope
-
