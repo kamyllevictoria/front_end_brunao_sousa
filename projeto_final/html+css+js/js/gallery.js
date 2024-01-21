eaa4f669d2293a752bc5663af45bf36d1c1a780e
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-01-20 21:23:51 -0300

    creating the next button and number counter in the gallery overlay

--- projeto_final/html+css+js/js/gallery.js
+++ projeto_final/html+css+js/js/gallery.js
@@ -3,13 +3,17 @@ var frameImage = document.querySelector('.jl-gallery-frame-image');
 var frameContainer = document.querySelector('.jl-gallery-frame-container');
 var galleryImages = document.querySelectorAll('.jl-thumb-img');
 var closeGallery = document.querySelectorAll('.jl-toggle-gallery');
+var btnPrev = document.querySelector('.jl-item-prev');
+var btnNext = document.querySelector('.jl-item-next');
+
 
 const getImageSrc = function (){
     for(var i = 0; i < galleryImages.length; i++){
         galleryImages[i].addEventListener('click', function(){
             var imageSrc = this.getAttribute('data-src');
+            var itemNum = this.getAttribute('data-item');
             frameImage.setAttribute('src', imageSrc);
-
+            frameImage.setAttribute('data-index', itemNum);
             overlay.classList.add('jl-is-open');
             frameContainer.classList.add('jl-is-open');
             frameContainer.classList.add('fade-up-animation')
@@ -29,10 +33,35 @@ for(var c = 0; c < closeGallery.length; c++){
 }
 
 
+const nextItem = function(){
+    //current frame
+    var currentItemNum = frameImage.getAttribute('data-index');
+    console.log(currentItemNum);
 
-//capture the current frame
+    //capture the next frame
+    var nexItemNum = parseInt(currentItemNum) + 1;
+    console.log(nexItemNum)
+
+    
+    //we create the loop and capture the item that matches th number next to teh current item
+    for(var n = 0; n < galleryImages.length; n++){
+        var item = galleryImages[n];
+        var itemNum = parseInt(item.getAttribute('data-item'));
+
+        if(itemNum === nexItemNum){
+            //get data src
+            var nextSrc = item .getAttribute('data-src');
+            var nextIndex = item.getAttribute('data-item')
+            //passing the data-src to the img tag
+            frameImage.setAttribute('src', nextSrc);
+            frameImage.setAttribute('data-index', nextIndex);
+        }       
+    }
+} 
 
 
-//capture the next frame = current + 1
+btnNext.addEventListener('click', function(){
+    nextItem();
+})
 
-//we create the loop and capture the item that matches th number next to teh current item
+
