7d13dd88f884dc3a476b8e105ba37470ef734667
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-01-20 22:03:34 -0300

    number counter formatting

--- projeto_final/html+css+js/js/gallery.js
+++ projeto_final/html+css+js/js/gallery.js
@@ -6,21 +6,40 @@ var closeGallery = document.querySelectorAll('.jl-toggle-gallery');
 var btnPrev = document.querySelector('.jl-item-prev');
 var btnNext = document.querySelector('.jl-item-next');
 
+var currentCounter = document.querySelector('.jl-current-slide');
+var totalCounter = document.querySelector('.jl-total-slide');
+
+
+
+//counter formatter
+var counterFormatterItem = function(j){
+    if(j < 10){
+        return '0' + j;
+    }
+        else{
+            return j;
+        }
+
+}
+
+totalCounter.innerHTML = counterFormatterItem(galleryImages.length);
 
 const getImageSrc = function (){
     for(var i = 0; i < galleryImages.length; i++){
         galleryImages[i].addEventListener('click', function(){
             var imageSrc = this.getAttribute('data-src');
             var itemNum = this.getAttribute('data-item');
+
             frameImage.setAttribute('src', imageSrc);
             frameImage.setAttribute('data-index', itemNum);
             overlay.classList.add('jl-is-open');
             frameContainer.classList.add('jl-is-open');
             frameContainer.classList.add('fade-up-animation')
+
+            currentCounter.innerHTML = counterFormatterItem(itemNum);
         })
     }
 }
-
 getImageSrc();
 
 //close overlay
@@ -40,9 +59,7 @@ const nextItem = function(){
 
     //capture the next frame
     var nexItemNum = parseInt(currentItemNum) + 1;
-    console.log(nexItemNum)
 
-    
     //we create the loop and capture the item that matches th number next to teh current item
     for(var n = 0; n < galleryImages.length; n++){
         var item = galleryImages[n];
@@ -55,13 +72,48 @@ const nextItem = function(){
             //passing the data-src to the img tag
             frameImage.setAttribute('src', nextSrc);
             frameImage.setAttribute('data-index', nextIndex);
+
+            currentCounter.innerHTML = counterFormatterItem(nextIndex);
+
         }       
+    }
+} 
+
+const prevItem = function(){
+    //current frame
+    var currentItemNum = frameImage.getAttribute('data-index');
+    console.log(currentItemNum);
+
+    //capture the next frame
+    var prevItemNum = parseInt(currentItemNum) - 1;
+
+    //we create the loop and capture the item that matches th number next to teh current item
+    for(var p = 0; p < galleryImages.length; p++){
+        var item = galleryImages[p];
+        var itemNum = parseInt(item.getAttribute('data-item'));
+
+        if(itemNum === prevItemNum){
+            //get data src
+            var prevSrc = item .getAttribute('data-src');
+            var prevIndex = item.getAttribute('data-item')
+            //passing the data-src to the img tag
+            frameImage.setAttribute('src', prevSrc);
+            frameImage.setAttribute('data-index', prevIndex);
+
+
+            currentCounter.innerHTML = counterFormatterItem(prevIndex);
+        }
+        
     }
 } 
 
 
 btnNext.addEventListener('click', function(){
     nextItem();
+})
+
+btnPrev.addEventListener('click', function(){
+    prevItem();
 })
 
 
